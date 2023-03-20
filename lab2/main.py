10c18966351ee6f939836de6e12251aa0a0404fa
kmashaa <m.kravcenko@yandex.by>
2023-03-20 03:21:23 +0300

    tests

--- lab2/main.py
+++ lab2/main.py
@@ -2,6 +2,76 @@
 import inv_matrix as inv
 import numpy as np
 
+def start_tests():
+    test1()
+    test2()
+    test3()
+    print("all tests passed successful")
+def test1():
+    cc = np.array([[1], [1], [0], [0], [0]])
+    AA = np.matrix([[-1, 1, 1, 0, 0], [1, 0, 0, 1, 0], [0, 1, 0, 0, 1]])
+    xx = np.array([[0], [0], [1], [3], [2]])
+    jj = np.array([3, 4, 5])
+    expected_res=np.array([[3],[2],[2],[0],[0]])
+
+    while True:
+        xx,jj,AAA_inv,ii,exx=lab(cc,AA,xx,jj)
+        if exx:
+            if(expected_res!=xx).any():
+                print("test failed")
+                break
+            else:
+                print("test passed successful")
+                break
+
+def test2():
+    cc = np.array([[1], [0], [0], [0]])
+    AA = np.matrix([[-1, 0, 1, 0], [0, -1, 0, 1]])
+    xx = np.array([[1], [1], [0], [0]])
+    jj = np.array([1, 2])
+    while True:
+        xx, jj, AAA_inv, ii, exx = lab(cc, AA, xx, jj)
+        if exx:
+            print("test passed successful")
+            break
+
+def test3():
+    cc = np.array([[20],
+                  [25],
+                  [0],
+                  [0],
+                  [0],
+                  [0],
+                  [0],
+                  [0]])
+    AA = np.matrix([[1, 2, 1, 0, 0, 0, 0, 0],
+                  [2, 1, 0, 1, 0, 0, 0, 0],
+                  [-1, 0, 0, 0, 1, 0, 0, 0],
+                  [1, 0, 0, 0, 0, 1, 0, 0],
+                  [0, -1, 0, 0, 0, 0, 1, 0],
+                  [0, 1, 0, 0, 0, 0, 0, 1]])
+    xx = np.array([[0],
+                  [0],
+                  [10],
+                  [11],
+                  [0],
+                  [5],
+                  [0],
+                  [4]])
+    jj = np.array([3,4,5,6,7,8])
+    expected_res=np.array([[4], [3], [0],[0],[4],[1],[3],[1]])
+    while True:
+        xx, jj, AAA_inv, ii, exx = lab(cc, AA, xx, jj)
+        if exx:
+            if (expected_res != xx).any():
+                print("test failed")
+                print(xx)
+                print(expected_res)
+                break
+            else:
+                print("test passed successful")
+                break
+
 def lab(c,A,x,j,A_inv=None,ii=None):
     ct=np.transpose(c)
     #step 1
@@ -14,7 +84,6 @@ def lab(c,A,x,j,A_inv=None,ii=None):
     else:
         xx=np.array(A[:,j[ii]-1])
         AB_inv=inv.find_inverse_matrix(np.array(A_inv),xx,ii)
-    print(AB_inv)
 
     #step 2
     cb=np.empty(shape=np.size(j))
@@ -30,7 +99,7 @@ def lab(c,A,x,j,A_inv=None,ii=None):
     lambt=np.dot(ut,A)-ct
 
     #step 5
-    if(np.amin(lambt)>=0):
+    if(int(np.amin(lambt))>=0):
         return x, j, AB_inv, i, 1
 
     #step 6
@@ -41,6 +110,7 @@ def lab(c,A,x,j,A_inv=None,ii=None):
 
     #step 8
     tettat=np.zeros(np.size(z))
+
     ind=0
     for zet in np.nditer(z):
         if zet<=0:
@@ -49,13 +119,14 @@ def lab(c,A,x,j,A_inv=None,ii=None):
             tettat[ind]=float(x[j[ind]-1])/float(z[ind])
         ind+=1
 
+
     #step 9
     tetta0=np.amin(tettat)
 
     #step 10
     if float(tetta0)==float('inf'):
         print("целевой функционал задачи не ограничен сверху на множестве допустимых планов")
-        exit(0)
+        return x, j, AB_inv, 1, 1
 
     #step 11
     k=np.where(tettat==tetta0)[0][0]
@@ -72,7 +143,6 @@ def lab(c,A,x,j,A_inv=None,ii=None):
             x[j[i]-1]=x[j[i]-1]-tetta0*z[i]
 
     x[jst-1]=0
-
     return x, j, AB_inv, k, 0
 
 
@@ -81,19 +151,8 @@ def lab(c,A,x,j,A_inv=None,ii=None):
 if __name__ == '__main__':
 
 
+    start_tests()
 
-
-
-    c=np.array([[1],[1],[0],[0],[0]])
-    A=np.matrix([[-1,1,1,0,0],[1,0,0,1,0],[0,1,0,0,1]])
-    x=np.array([[0],[0],[1],[3],[2]])
-    j=np.array([3,4,5])
-    ex=0
-    while True:
-        x,j,AA_inv,i,ex=lab(c,A,x,j)
-        if ex:
-            print(x)
-            exit(0)
 
 
 
